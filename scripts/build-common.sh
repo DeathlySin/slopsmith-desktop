f11fcff43234ce25947771d1ded1f8d003973e38
Sin <deathlysin@outlook.com>
2026-05-28 22:02:46 +0100

    fix: formatting, move it inline with the other plugins.

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -146,7 +146,7 @@ clone_slopsmith() {
 		# Community plugins
 		alleexx/slopsmith-plugin-transpose-chords
 		ChrisBeWithYou/slopsmith-plugin-slopscale
-    DeathlySin/slopsmith-plugin-song-preview
+		DeathlySin/slopsmith-plugin-song-preview
 		Jafz2001/slopsmith-plugin-nam-rig-builder
 		masc0t/slopsmith-plugin-find-more
 		masc0t/slopsmith-plugin-invert-highway
